69b3cfff027a1113ac2db58298cbb5376ba8ea39
Ryan Williams <ryan.williams@cogizant.com>
2020-06-19 07:20:25 -0500

    initial commit

---
+++ BDR/src/main/java/com/codereese/BDR/ComedyApiApplication.java
@@ -0,0 +1,13 @@
+package com.codereese.BDR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComedyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComedyApiApplication.class, args);
+	}
+
+}
---
+++ BDR/src/test/java/com/codereese/BDR/ComedyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codereese.BDR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComedyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
