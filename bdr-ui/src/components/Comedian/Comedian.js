7dbb3b49a7bf6adcdcb920b79bca9712a89ac384
Ryan Williams <ryan.williams@cogizant.com>
2020-06-20 19:51:13 -0500

    hitting the apis from the front

--- bdr-ui/src/components/Comedian/Comedian.js
+++ bdr-ui/src/components/Comedian/Comedian.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
 import './Comedian.css';
+import comedianService from '../../services/comedianService.js';
 
 class Comedian extends Component {
     constructor(props){
         super(props);
+       
+    }
+   
+    componentDidMount(){
+        this.comedianList =  comedianService.fetchComedians();
+       console.log(this.comedianList.then(resolve => resolve));
+        // this.comedianList.map(comedian => {
+        //     return comedian;
+        // })
     }
 
     render(){
--- bdr-ui/src/services/comedianService.js
+++ bdr-ui/src/services/comedianService.js
@@ -1,14 +1,26 @@
 const SERVER = "http://localhost:8081/comedians";
 
 const comedianService = {
-    async fetchComedians(){
-        return await fetch(SERVER, {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials:'same-origin'
+
+    testWire(){
+        console.log("WIRED CORRECTLY");
+    },
+
+    // async fetchComedians(){
+    //     return await fetch(SERVER, {
+    //         method: 'GET',
+    //         mode: 'cors',
+    //         cache: 'no-cache',
+    //         credentials:'same-origin'
             
-        })
+    //     })
+    //     .then(response => response)
+    //     .catch(err => console.log("error: ", err));
+    // }
+    async fetchComedians(){
+        return fetch(SERVER)
+        .then(response => response.json())
+        .then(data => console.log(data));
     }
 }
 
